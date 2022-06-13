ff04f7537a017c5bd59fea9607ff4faf59c96ddf
Norayr <norayrkaloyan@yahoo.com>
2022-06-13 16:05:00 +0200

    first structur commit

---
+++ routes/protectedRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getUserInfo } from "../controllers/users.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+const protectedRoute = express.Router();
+
+protectedRoute.get("/me", verifyToken, getUserInfo);
+
+export default protectedRoute;
